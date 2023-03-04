4f017150a3e0bf09f7ad6b71f84822ff3feb849c
Cirrus CI <support@cirruslabs.org>
2023-03-04 12:06:10 +0000

    ae

---
+++ lib/blocs/bloc/counter_bloc.dart
@@ -0,0 +1,13 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+
+part 'counter_event.dart';
+part 'counter_state.dart';
+
+class CounterBloc extends Bloc<CounterEvent, CounterState> {
+  CounterBloc() : super(CounterInitial()) {
+    on<CounterIncrease>((event, emit) {
+      emit(CounterChanged(counter: state.counter + 1));
+    });
+  }
+}
